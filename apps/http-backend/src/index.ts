6121b4a83e544354a21e5dafa8a589fc7de68b1d
Appr0ximity <hvshekharmurthy@gmail.com>
2025-09-09 08:48:56 +0530

    DB connection

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,47 +1,109 @@
 import express from "express"
-import {signUpSchema, signInSchema} from "@repo/validations"
+import {signUpSchema, signInSchema, roomSchema} from "@repo/validations"
+import jwt, { TokenExpiredError } from "jsonwebtoken"
+import { JWT_TOKEN } from "@repo/common"
+import { prismaClient } from "@repo/db"
+
+interface AuthenticatedRequest extends express.Request{
+    user?:{
+        id: String,
+        username: String
+    }
+}
 
 const app = express();
 
-app.post("signin", (req,res)=>{
+app.use(express.json())
+
+function authenticateToken (req: AuthenticatedRequest, res: express.Response){
+    const authHeaders = req.headers["authorization"]
+    const token = authHeaders && authHeaders.split(' ')[1]
+
+    if(!token){
+        return res.status(400).json({
+            message: "No token found!"
+        })
+    }else{
+        jwt.verify(token, JWT_TOKEN, (err: any, user: any)=>{
+            if(err){
+                return res.status(403).json({
+                    message:"Could not verify the jwt"
+                })
+            }else{
+                req.user = user
+            }
+        })
+    }
+}
+
+app.post("signin", async (req,res)=>{
     if(signInSchema.safeParse(req.body).success){
         const {username, password} = req.body;
-        if(0){ //username doesn't exist in the db
-            res.status(400).json({
+
+        const user = await prismaClient.user.findUnique({
+            where: {
+                username: username
+            }
+        })
+
+        if(user === null){ //username doesn't exist in the db
+            return res.status(400).json({
                 message : `${username} doesn't exist`
             })
         }
-        if(0){//username and password doesn't match
-            res.status(400).json({
+        if(user.password !== password){//username and password doesn't match
+            return res.status(400).json({
                 message : "username and password doesn't match"
             })
         }
-        if(1){ //username and password matches
-            res.status(200).json({
-                message: "Login successful!"
-            })
-        }
+
+        const token = jwt.sign({
+            id: user.id
+        }, JWT_TOKEN)
+
+        return res.status(200).json({
+            message: "Login successful!",
+            token: token
+        })
     }else{
-        res.status(400).json({
+        return res.status(400).json({
             message: "Invalid Credentials"
         })
     }
 })
 
-app.post("signup", (req,res)=>{
+app.post("signup", async (req,res)=>{
 
     if(signUpSchema.safeParse(req.body).success){
-        const {username, password} = req.body;
-        if(0){//username already exists
-            res.status(400).json({
+        const {username, password, email} = req.body;
+
+        const existingUser = prismaClient.user.findUnique({
+            where: {
+                username: username
+            }
+        })
+
+        if(existingUser){//username already exists
+            return res.status(400).json({
                 message : "username already exists!!"
             })
         }
-        if(1){
-            res.status(400).json({
-                message : "Signup successful!"
-            })
-        }
+        const newUser = await prismaClient.user.create({
+            data: {
+                username: username,
+                password: password,
+                email: email
+            }
+        })
+
+        const token = jwt.sign({
+            id: newUser.id
+        }, JWT_TOKEN)
+
+        return res.status(400).json({
+            token: token,
+            message : "Signup successful!"
+        })
     }else{
         return res.status(400).json({
             message : "Invalid credentials!"
@@ -49,8 +111,43 @@ app.post("signup", (req,res)=>{
     }
 })
 
-app.post("create-room", (req,res)=>{
-    
+app.post("create-room", authenticateToken, async (req: AuthenticatedRequest,res)=>{
+    if(roomSchema.safeParse(req.body).success){
+        if (!req.user) {
+            return res.status(401).json({
+                message: "Unauthorized: User not authenticated"
+            });
+        }
+        const {slug} = req.body
+
+        const existingRoom = await prismaClient.room.findUnique({
+            where:{
+                slug: slug
+            }
+        })
+        if(existingRoom !== null){
+            return res.status(400).json({
+                message: "Provide a different room password"
+            })
+        }
+
+        
+        const newRoom = prismaClient.room.create({
+            data:{
+                slug: slug,
+                admin: req.user.id
+            }
+        })
+
+        return res.status(200).json({
+            message: "Room created successfully!",
+            room: newRoom
+        })
+    }else{
+        return res.status(400).json({
+            message: "Invalid room password sent"
+        })
+    }
 })
 
 app.listen(3000)
